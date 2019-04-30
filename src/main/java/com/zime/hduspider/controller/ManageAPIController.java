eee7fd6b58fa595b68d35b03d6a19cb2a4eda9b2
fanliyong007 <fanliyong007@hotmail.com>
2019-04-30 22:56:52 +0800

    代码完成

--- src/main/java/com/zime/hduspider/controller/ManageAPIController.java
+++ src/main/java/com/zime/hduspider/controller/ManageAPIController.java
@@ -7,13 +7,17 @@ import com.zime.hduspider.service.WordService;
 import com.zime.hduspider.utils.GoPY;
 import com.zime.hduspider.utils.Msg;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.FileSystemResource;
+import org.springframework.core.io.InputStreamResource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
 import javax.validation.Valid;
+import java.io.IOException;
 import java.util.List;
 
 @RestController
@@ -22,31 +26,68 @@ public class ManageAPIController
 {
     @Autowired
     private WordService wordService;
+
     @RequestMapping(value = "/word")
     public Msg getALL(@RequestParam(value = "pn", defaultValue = "1") Integer pageNum)
     {
         //在查询之前调用静态方法设置起始页和页面大小
-        PageHelper.startPage(pageNum, 100,"Frequency desc");
+        PageHelper.startPage(pageNum, 100, "Frequency desc");
         //startPage后面紧跟着的查询就是分页查询
-        List<Word> words=wordService.getAll();
+        List<Word> words = wordService.getAll();
         //使用PageInfo包装查询后的结果，并将pageInfo存入map中
-        PageInfo<Word> wordPageInfo=new PageInfo<Word>(words,5);
-        return Msg.success().add("word",wordPageInfo);
+        PageInfo<Word> wordPageInfo = new PageInfo<Word>(words, 5);
+        return Msg.success().add("word", wordPageInfo);
     }
+
     @RequestMapping("/save")
     public Msg saveCode(@Valid User user, BindingResult result)
     {
         try
         {
             System.out.println(user.toString());
-            GoPY goPY=new GoPY();
-            goPY.exec(user.getUsername(),user.getPassword(),"D:\\hduspider\\src\\main\\resources\\Spider\\testPy.py");
-            Thread.sleep(30000);
-
+            GoPY goPY = new GoPY();
+            goPY.exec(user.getUsername(), user.getPassword(), "D:\\hduspider\\src\\main\\resources\\Spider\\HDUSpider.py");
             return Msg.success();
-        } catch (Exception e)
+        }
+        catch (Exception e)
         {
             return Msg.fail().add("errors", e.getMessage());
         }
+    }
+    @GetMapping("/download")
+    public ResponseEntity<InputStreamResource> downloadFile(Long id)
+            throws IOException
+    {
+        String filePath = "D:\\hduspider\\src\\main\\resources\\Spider\\tmp.zip";
+        FileSystemResource file = new FileSystemResource(filePath);
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Cache-Control", "no-cache, no-store, must-revalidate");
+        headers.add("Content-Disposition", String.format("attachment; filename=\"%s\"", file.getFilename()));
+        headers.add("Pragma", "no-cache");
+        headers.add("Expires", "0");
+        return ResponseEntity
+                .ok()
+                .headers(headers)
+                .contentLength(file.contentLength())
+                .contentType(MediaType.parseMediaType("application/octet-stream"))
+                .body(new InputStreamResource(file.getInputStream()));
+    }
+    @GetMapping("/question")
+    public ResponseEntity<InputStreamResource> downloadQuestion(Long id)
+            throws IOException
+    {
+        String filePath = "D:\\hduspider\\src\\main\\resources\\Spider\\question.zip";
+        FileSystemResource file = new FileSystemResource(filePath);
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Cache-Control", "no-cache, no-store, must-revalidate");
+        headers.add("Content-Disposition", String.format("attachment; filename=\"%s\"", file.getFilename()));
+        headers.add("Pragma", "no-cache");
+        headers.add("Expires", "0");
+        return ResponseEntity
+                .ok()
+                .headers(headers)
+                .contentLength(file.contentLength())
+                .contentType(MediaType.parseMediaType("application/octet-stream"))
+                .body(new InputStreamResource(file.getInputStream()));
     }
 }
--- src/main/resources/Spider/HDUSpider.py
+++ src/main/resources/Spider/HDUSpider.py
@@ -1,13 +1,11 @@
 # coding = utf-8
-import re
-import os
-import shutil
+import os,sys
 import zipfile
-from os.path import join, getsize
 from selenium import webdriver
+Myque=[]
 option = webdriver.ChromeOptions()
 option.add_argument("headless")
-browser = webdriver.Chrome(chrome_options=option)
+browser = webdriver.Chrome(options=option)
 def zip_file(src_dir):
     zip_name = src_dir +'.zip'
     z = zipfile.ZipFile(zip_name,'w',zipfile.ZIP_DEFLATED)
@@ -18,32 +16,47 @@ def zip_file(src_dir):
             z.write(os.path.join(dirpath, filename),fpath+filename)
             print ('==压缩成功==')
     z.close()
-username=input("")#读入账户
-userpass=input("")#读入密码
-browser.get("http://acm.hdu.edu.cn/userstatus.php?user="+username)#取得当前用户题目集
-queXpath="/html/body/table/tbody/tr[4]/td/table/tbody/tr/td/p[3]"#题目集xpath路径
+def removeFileInFirstDir(targetDir):
+    for file in os.listdir(targetDir):
+        targetFile = os.path.join(targetDir,  file)
+        if os.path.isfile(targetFile):
+            os.remove(targetFile)
+username=input()#读入账户
+userpass=input()#读入密码
+browser.get("http://acm.hdu.edu.cn/status.php?user="+username)#取得当前用户题目集
 browser.find_element_by_name("username").send_keys(username)
 browser.find_element_by_name("userpass").send_keys(userpass)
 browser.find_element_by_name("login").click()#模拟登录
-que=browser.find_element_by_xpath(queXpath)#获取题目集文本
-Myque=[]
-for str in que.text.split(' '):
-    if re.match(r'\d{4}',str):
-        Myque.append(str)
-for queNum in Myque:
-    browser.get("http://acm.hdu.edu.cn/status.php?user="+username+"&pid="+queNum+"&status=5")
-    browser.find_element_by_xpath("//*[@id=\"fixed_table\"]/table/tbody/tr[2]/td[7]/a").click()
-    hander=browser.current_window_handle
-    handers=browser.window_handles
-    for h in handers:
-        if h!=hander:
-            browser.switch_to_window(h)
-            code = browser.find_element_by_xpath("/html/body/table/tbody/tr[4]/td/div/div[2]/pre")
-            print(code.text)
-            f=open('D:\hduspider\src\main\resources\Spider\tmp\\'+queNum,'w')
-            f.write(code.text)
-            f.close()
-            browser.close()
-            browser.switch_to_window(hander)
-zip_file("D:\hduspider\src\main\resources\Spider\tmp\\")
+flag=True
+while flag:
+    table=browser.find_element_by_id("fixed_table")
+
+    for i in range(2,17):
+        try:
+            xpath="//table/tbody/tr[" + str(i) + "]/td[3]/font"
+            status = table.find_element_by_xpath(xpath)
+        except:
+            try:
+                xpath = "//table/tbody/tr[" + str(i) + "]/td[3]/a/font"
+                status = table.find_element_by_xpath(xpath)
+            except:
+                flag=False
+        if flag:
+            if status.text=="Accepted":
+                ans=table.find_element_by_xpath("//table/tbody/tr["+str(i)+"]/td[7]/a").get_attribute("href")
+                Myque.append(ans)
+    browser.find_element_by_xpath("//*[@id=\"fixed_table\"]/p/a[3]").click()
+Myque=set(Myque)
+for quePath in Myque:
+    browser.get(quePath)
+    code = browser.find_element_by_xpath("/html/body/table/tbody/tr[4]/td/div/div[2]/pre")
+    queNum=browser.find_element_by_xpath("/html/body/table/tbody/tr[4]/td/div/div[1]/b/font/a[1]").text
+    print(code.text)
+    filename=sys.path[0]+"\\tmp\\"+queNum.replace('==',' equals ').replace('?'," wen ")
+    f=open(filename,'w')
+    f.write(code.text)
+    f.close()
+browser.close()
+zip_file(sys.path[0]+"\\tmp")
+removeFileInFirstDir(sys.path[0]+"\\tmp")
 browser.quit()
